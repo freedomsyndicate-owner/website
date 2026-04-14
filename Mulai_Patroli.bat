310158760a2deb28b02a4454284f168e66953fcd
freedomsyndicate-owner <officiallisan230717@gmail.com>
2026-04-15 02:37:51 +0800

    Create Mulai_Patroli.bat

---
+++ Mulai_Patroli.bat
@@ -0,0 +1,14 @@
+@echo off
+title DOMS MULTI-PROCESSOR V10
+echo 🦅 MENGAKTIFKAN SISTEM FREEDOM SYNDICATE... 🦅
+echo.
+
+:: Menjalankan Bot Predator (Teknikal XAMD)
+start "DOMS PREDATOR" py bot_predator.py
+
+:: Menjalankan Bot Fundamental (Omniscience Intel)
+start "DOMS INTEL" py bot_fundametal.py
+
+echo ✅ SEMUA SISTEM AKTIF! JANGAN TUTUP CMD INI.
+echo 🚀 SELAMAT BERTAWURAN DENGAN MARKET, COK!
+pause
